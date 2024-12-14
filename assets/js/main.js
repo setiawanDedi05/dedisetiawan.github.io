a5d9309c80b1df047d0e577c43b328d59ccd1c08
setiawanDedi05 <dssetiawan05@gmail.com>
2024-12-14 10:56:21 +0700

    add responsive and reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,36 +26,114 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
 const showHeader = () => {
-    const header = document.getElementById('header')
-    this.scrollY >= 50  ? header.classList.add('shadow-header') : header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll', showHeader)
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", showHeader);
 
 /*=============== EMAIL JS ===============*/
-const contactForm = document.getElementById('contact-form'),
-        contactMessage = document.getElementById("contact-message")
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
 
 const sendEmail = (e) => {
-    e.preventDefault()
-    emailjs.sendForm('service_abejysr','template_g9677cq', '#contact-form', '8ka5pMIywnqyFZlFq').then(() => {
-        contactMessage.textContent = 'Message has been sent'
+  e.preventDefault();
+  emailjs
+    .sendForm(
+      "service_abejysr",
+      "template_g9677cq",
+      "#contact-form",
+      "8ka5pMIywnqyFZlFq"
+    )
+    .then(
+      () => {
+        contactMessage.textContent = "Message has been sent";
 
         setTimeout(() => {
-            contactMessage.textContent = ""
-        }, 5000)
+          contactMessage.textContent = "";
+        }, 5000);
 
-        contactForm.reset()
-    }, () => {
-        contactMessage.textContent = "Message failed to sent"
-    })
-}
+        contactForm.reset();
+      },
+      () => {
+        contactMessage.textContent = "Message failed to sent";
+      }
+    );
+};
 
-contactForm.addEventListener('submit', sendEmail)
+contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
 
+window.addEventListener("scroll", scrollUp);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id");
 
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true,
+});
+
+sr.reveal(".home__perfil, .about__image, .contact__mail", { origin: "right" });
+sr.reveal(".home__name, .home__info, .about__container .section__title-1, .about__info", { origin: "left" });
+sr.reveal(".services__card, .projects__card", {interval: 100})
+sr.reveal(".contact__container", {interval: 100})
